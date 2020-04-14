db67453254cb9e0cdc1d7bcdbc0bf70dcdd20ba6
Ivan Savin <acccko@gmail.com>
2020-04-14 21:44:49 +0100

    commit for deployment https://nostalgic-jang-986355.netlify.com

---
+++ build/precache-manifest.a700ea6d14c3a49e32113c0bd3f7727c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8977c6943e5565762a25d85d4ae959a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be7839140dee8d6ff7f4",
+    "url": "/static/css/2.9dafe002.chunk.css"
+  },
+  {
+    "revision": "a03192d0f9766b1b3bda",
+    "url": "/static/css/main.20034467.chunk.css"
+  },
+  {
+    "revision": "be7839140dee8d6ff7f4",
+    "url": "/static/js/2.1d691337.chunk.js"
+  },
+  {
+    "revision": "a03192d0f9766b1b3bda",
+    "url": "/static/js/main.fd6a4a4e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
